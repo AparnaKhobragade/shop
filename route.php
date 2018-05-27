0897a09017663c3a70ec5f824ba87ea1ba5c1a8b
Aparna <aparna.nagdeote@gmail.com>
2018-05-28 00:04:23 +0530

    Update route.php

--- route.php
+++ route.php
@@ -1,5 +1,5 @@
 <?php
-$site_url = 'http://localhost/blog/';
+$site_url = 'http://localhost/shop/';
 $current_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 $request = str_replace($site_url,'' , $current_url);
 ?>
